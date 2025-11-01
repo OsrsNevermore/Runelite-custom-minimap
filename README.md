a286412836b0c87c3daa29b2cc7a21d09a257db8
OsrsNevermore <241385807+OsrsNevermore@users.noreply.github.com>
2025-11-01 09:11:01 -0100

    Plugin Creation
    Introduce Widget Hiding and Orb Translation

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# Example
-An example greeter plugin
+# Custom Minimap
+
+This plugin allows you to shift the position of your orbs, and toggle the visibility of all minimap items.
+
+You can also enable clicking through the minimap and compass.
+
+
+
+NOTE: If you turn off the plugin while orbs are shifted, they will remain in their custom location until you minimize/maximize your minimap with the world orb. You can also manually reset them to fix this.
+
--- src/test/java/com/orbhidermini/OrbHiderminiPluginTest.java
+++ src/test/java/com/orbhidermini/OrbHiderminiPluginTest.java
@@ -1,13 +1,13 @@
-package com.example;
+package com.orbhidermini;
 
 import net.runelite.client.RuneLite;
 import net.runelite.client.externalplugins.ExternalPluginManager;
 
-public class ExamplePluginTest
+public class OrbHiderminiPluginTest
 {
 	public static void main(String[] args) throws Exception
 	{
-		ExternalPluginManager.loadBuiltin(ExamplePlugin.class);
+		ExternalPluginManager.loadBuiltin(OrbHiderminiPlugin.class);
 		RuneLite.main(args);
 	}
 }
